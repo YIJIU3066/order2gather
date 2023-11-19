e5d993d0284e717cf27b3f5f2615e784b992d728
YIJIU3066 <yijiu3066@gmail.com>
2023-11-19 22:05:10 +0800

    navbar ui

---
+++ src/components/navbar.js
@@ -0,0 +1,137 @@
+import React, { useState } from "react";
+import styles from "../styles/navbar.module.css";
+
+const NavBar = () => {
+  const [isAboutMeOpen, setIsAboutMeOpen] = useState(false);
+  const [isOrderOpen, setIsOrderOpen] = useState(false);
+
+  const toggleDropdown = (item) => {
+    if (item === "About") {
+      setIsAboutMeOpen(!isAboutMeOpen);
+    } else if (item === "Order") {
+      setIsOrderOpen(!isOrderOpen);
+    }
+  };
+
+  return (
+    <nav className="bg-blue w-full">
+      <div className="max-w-screen-xl flex items-center justify-between mx-2 p-2">
+        <div className="flex w-2/3 items-center " id="navbar-left">
+          <img src="/images/house.png" className="h-6" alt="House" />
+          <div className="flex relative" id="navbar-dropdown">
+            <ul className="flex font-medium mx-6 rtl:space-x-reverse">
+              <li>
+                <button
+                  id="dropdownNavbarLink"
+                  onClick={() => toggleDropdown("About")}
+                  className={`${styles.dropdown}`}
+                >
+                  About Me{" "}
+                  <svg
+                    className="w-2.5 h-2.5 ms-2.5"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 10 6"
+                  >
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="m1 1 4 4 4-4"
+                    />
+                  </svg>
+                </button>
+                <div
+                  id="dropdownNavbar"
+                  className={`z-10 ${
+                    isAboutMeOpen ? "block" : "hidden"
+                  } font-normal bg-white divide-y divide-gray-100 rounded-lg shadow`}
+                >
+                  <ul
+                    className="pt-2 text-base text-white bg-blue absolute"
+                  >
+                    <li>
+                      <div className={`${styles.dropdown_item}`}>
+                        My Restaurant
+                      </div>
+                    </li>
+                    <li>
+                      <div className={`${styles.dropdown_item}`}>
+                        My Friend & Group
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+
+              <li>
+                <button
+                  id="dropdownNavbarLink"
+                  onClick={() => toggleDropdown("Order")}
+                  className={`${styles.dropdown} ml-14`}
+                >
+                  Order{" "}
+                  <svg
+                    className="w-2.5 h-2.5 ms-2.5"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 10 6"
+                  >
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="m1 1 4 4 4-4"
+                    />
+                  </svg>
+                </button>
+                <div
+                  id="dropdownNavbar"
+                  className={`z-10 ${
+                    isOrderOpen ? "block" : "hidden"
+                  } font-normal bg-white divide-y divide-gray-100 rounded-lg shadow`}
+                >
+                  <ul
+                    className="pt-2 text-base text-white bg-blue absolute ml-14"
+                  >
+                    <li>
+                      <div className={`${styles.dropdown_item}`}>
+                        Create Order
+                      </div>
+                    </li>
+                    <li>
+                      <div className={`${styles.dropdown_item}`}>
+                        Order History
+                      </div>
+                    </li>
+                    <li>
+                      <div className={`${styles.dropdown_item}`}>
+                        View Order
+                      </div>
+                    </li>
+                    <li>
+                      <div className={`${styles.dropdown_item}`}>
+                        Order Report
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="w-1/3" id="navbar-login">
+          <div className="flex flex-row-reverse items-center justify-between w-full py-2 px-3 text-lg font-medium text-white hover:text-gray-700 cursor-pointer rounded z">
+            Log in
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
--- src/containers/home.js
+++ src/containers/home.js
@@ -1,8 +1,12 @@
+import NavBar from "../components/navbar";
+import React from "react";
 
 const Home = () => {
   return (
     <>
+      <NavBar/>
       <div className="text-2xl text-blue">首頁內容</div>
+    
     </>
   );
 };
