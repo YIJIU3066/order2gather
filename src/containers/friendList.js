21a3841c60835a77abdefd4951d7145196d8cd5f
Weber Chang <s0975247623@gmail.com>
2023-11-22 19:35:08 +0800

    Add and delete friend functionality with mock data

--- src/containers/friendList.js
+++ src/containers/friendList.js
@@ -2,22 +2,34 @@ import React, { useState, useEffect } from 'react';
 import Navbar from "../components/navbar";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUserGroup } from '@fortawesome/free-solid-svg-icons';
+import ListNav from "../components/listNav"
+import AddFriendForm from '../components/addFriendForm';
+
+const mockGroups = ["Cook", "Teacher", "Boss", "Gang", "Chicken", "Lawyer", "DEA"]
 
 const mockFriends = [
     {nickname: 'Walter', gmail: "chemistryisart@gmail.com", groups: ["Cook", "Teacher", "Boss"], checked: false},
     {nickname: 'Jesse', gmail: "yoyoyo@gmail.com", groups: ["Cook", "Gang"], checked: false},
-    {nickname: 'Chicken Man', gmail: "lospoloshermanos@gmail.com", groups: ["Boss"], checked: false},
+    {nickname: 'Gus', gmail: "lospoloshermanos@gmail.com", groups: ["Boss", "Chicken"], checked: false},
     {nickname: 'Skyler', gmail: "mynameisskylerwhiteyo@gmail.com", groups: [], checked: false},
-    {nickname: 'Saul', gmail: "bettercallsaul@gmail.com", groups: ["Lawyer"], checked: false}
+    {nickname: 'Saul', gmail: "bettercallsaul@gmail.com", groups: ["Lawyer"], checked: false},
+    {nickname: 'Hank', gmail: "coolmineral@gmail.com", groups: ["DEA"], checked: false},
+    {nickname: 'Mike', gmail: "waltuh@gmail.com", groups: ["Chicken"], checked: false},
 ]
 
 export default function FriendList() {
 
     const [friendList, setFriendList] = useState([]);
+    const [showFriends, setShowFriends] = useState([0, 5]);
+    const [addOpen, setAddOpen] = useState(false);
 
     useEffect(() => {
         setFriendList(mockFriends);
     }, [])
+
+    const addFriend = (newFriend) => {
+        setFriendList(prevList => [...prevList, newFriend]);
+    }
 
     const handleClick = (e) => {
         const key = e.target.value;
@@ -29,6 +41,14 @@ export default function FriendList() {
             })
             return newList
         })
+    }   
+
+    const handleClickAdd = () => {
+        setAddOpen(true);
+    }
+
+    const handleDelete = () => {
+        setFriendList(friendList.filter(it => !it.checked));
     }
 
     return (
@@ -39,40 +59,39 @@ export default function FriendList() {
             </div>
             <div className="grid grid-cols-5 gap-2 px-[10vw] justify-items-center">
                 <button>
-                    <div className="min-w-fit text-3xl text-white bg-yellow w-8 h-8 rounded-full flex items-center justify-center">
+                    <div className="min-w-fit text-3xl text-white bg-yellow w-8 h-8 rounded-full flex items-center justify-center" onClick={handleClickAdd}>
                         <p className="mb-2">+</p>
                     </div>
                 </button>
                 <p className="text-2xl font-bold text-blue">Nickname</p>
                 <p className="text-2xl font-bold text-blue">Gmail</p>
                 <p className="text-2xl font-bold text-blue">In Group</p>
-                <div>
-                    <button className="text-3xl font-bold text-blue">{"<"}</button>
-                    <button className="text-3xl font-bold text-blue">&nbsp; {">"}</button>
-                </div>
+                <ListNav showIndex={showFriends} setShowIndex={setShowFriends} len={friendList.length}/>
             </div>
             <hr class="my-2 h-0.5 mx-[10vw] border-t-0 bg-grey opacity-20" />
             <>
                 {
-                    friendList.map((friend) => {
-                        return (
-                            <>
-                                <div class="grid grid-cols-5 gap-2 px-[10vw] justify-items-center items-center">
-                                    <input type="checkbox" value={friend.gmail} checked={friend.checked} onChange={handleClick} className="accent-blue w-4 h-4 border-blue" />
-                                    <p className="text-xl text-blue font-bold">{friend.nickname}</p>
-                                    <p className="text-lg break-all text-blue">{friend.gmail}</p>
-                                    <div className="flex flex-wrap">
-                                        {
-                                            friend.groups.map((group) => {
-                                                return (<div className="bg-yellow text-white rounded-lg p-1 m-1 font-bold text-center">{group}</div>)
-                                            })
-                                        }
+                    friendList.map((friend, index) => {
+                        if (index < showFriends[1] && index >= showFriends[0]) {
+                            return (
+                                <>
+                                    <div class="grid grid-cols-5 gap-2 px-[10vw] justify-items-center items-center">
+                                        <input type="checkbox" value={friend.gmail} checked={friend.checked} onChange={handleClick} className="accent-blue w-4 h-4 border-blue" />
+                                        <p className="text-xl text-blue font-bold">{friend.nickname}</p>
+                                        <p className="text-lg break-all text-blue">{friend.gmail}</p>
+                                        <div className="flex flex-wrap">
+                                            {
+                                                friend.groups.map((group) => {
+                                                    return (<div className="bg-yellow text-white rounded-lg p-1 m-1 font-bold text-center">{group}</div>)
+                                                })
+                                            }
+                                        </div>
+                                        <FontAwesomeIcon icon={faUserGroup} style={{color: "#7A989A"}} size="xl" />
                                     </div>
-                                    <FontAwesomeIcon icon={faUserGroup} style={{color: "#7A989A"}} size="xl" />
-                                </div>
-                                <hr class="my-2 h-0.5 mx-[10vw] border-t-0 bg-grey opacity-20" />
-                            </>
-                        )
+                                    <hr class="my-2 h-0.5 mx-[10vw] border-t-0 bg-grey opacity-20" />
+                                </>
+                            )
+                        }
                     })
                 }
             </>
@@ -81,15 +100,18 @@ export default function FriendList() {
                 <div></div>
                 <div></div>
                 <div></div>
-                <div>
-                    <button className="text-3xl font-bold text-blue">{"<"}</button>
-                    <button className="text-3xl font-bold text-blue">&nbsp; {">"}</button>
-                </div>
+                <ListNav showIndex={showFriends} setShowIndex={setShowFriends} len={friendList.length}/>
             </div>
             <div className="grid grid-cols-2 gap-2 px-[10vw] justify-items-center pt-5">
                 <button className='h-10 w-32 text-white text-xl font-bold bg-yellow rounded-lg'>Finish</button>
-                <button className='h-10 w-32 text-white text-xl font-bold bg-red rounded-lg'>Delete</button>
+                <button className='h-10 w-32 text-white text-xl font-bold bg-red rounded-lg' onClick={handleDelete}>Delete</button>
             </div>
+
+            {addOpen && (
+                <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-black bg-opacity-50 duration-100">
+                    <AddFriendForm groups={mockGroups} setAddOpen={setAddOpen} addFriend={addFriend}/>
+                </div>
+            )}
         </>
     )
 }
