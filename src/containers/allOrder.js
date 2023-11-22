61b410b2f2d8ee1222fd4df3e0b7329165ff99c4
Alexhtchen <b09902126@csie.ntu.edu.tw>
2023-11-22 23:58:02 +0800

    add initial dashboard

--- src/containers/allOrder.js
+++ src/containers/allOrder.js
@@ -5,12 +5,23 @@ const AllOrder = () => {
 
     const [openVarValue, setOpenVarValue] = useState(0);
     const [deliverVarValue, setDeliverVarValue] = useState(0);
+    const [myName, setMyName] = useState("Host 1");
     
     const navigate = useNavigate();
 
-    const handleRowClick = (id) => {
+    const handleOpenClick = (id) => {
         // 導航至相應的詳細頁面，假設路由設置為 `/historyOrderDetail/:id`
         navigate(`/historyOrderDetail/${id}`);
+    };
+
+    const handleDeliverClick = (id) => {
+        // 導航至相應的詳細頁面，假設路由設置為 `/historyOrderDetail/:id`
+        navigate(`/historyOrderDetail/${id}`);
+    };
+
+    const handleDashboardClick = (id) => {
+        // 導航至相應的詳細頁面，假設路由設置為 `/hostDashboard/:id`
+        navigate(`/hostDashboard/${id}`);
     };
 
     function decreaseOpenButtonClick (){
@@ -41,7 +52,7 @@ const AllOrder = () => {
             restaurant: "Restaurant BBB",
             openTime: "2023-07-29 11:00",
             deliverTime: "2023-07-29 13:00",
-            host: "Host 1",
+            host: "Host 2",
         },
         {
             id: 2,
@@ -129,14 +140,14 @@ const AllOrder = () => {
                 <p>Displaying items with id: {openVarValue}, {openVarValue + 1}, and {openVarValue + 2}</p>
                 
                 
-                <div className="flex p-1 items-center justify-around flex-wrap gap-6">
+                <div className="flex p-1 items-start justify-around flex-wrap gap-6">
                     <button onClick={decreaseOpenButtonClick}>Decrease</button>
                     {filteredOpenOrder.map(item => (
                         <div key={item.id} className="flex flex-col items-start">
                         <tr
                         className="border-b hover:text-white cursor-pointer"
                         key={item.id}
-                        onClick={() => handleRowClick(item.id)}
+                        onClick={() => handleOpenClick(item.id)}
                         >
                             <div
                                 className={`p-0 pl-1 pr-20 w-180 h-24 items-start ${
@@ -150,7 +161,7 @@ const AllOrder = () => {
                                 }`}
                             >
                             <strong className="items-start p-0"></strong> {item.restaurant}
-                        </div>
+                            </div>
                         </tr>
                         
                         <ul className="mt-2">
@@ -164,6 +175,21 @@ const AllOrder = () => {
                             <strong>Host:</strong> {item.host}
                           </li>
                         </ul>
+                        {item.host === myName && (
+                            <div key={item.id} className="flex flex-col items-center">
+                                <tr
+                                className="border-b hover:text-white cursor-pointer"
+                                key={item.id}
+                                onClick={() => handleDashboardClick(item.id)}
+                                >
+                                <div
+                                    className={"p-0 pl-1 pr-20 w-160 h-18 items-center bg-red"}
+                                >
+                                    <div className="items-start p-0">Dashboard</div>
+                                </div>
+                                </tr>
+                            </div>
+                            )}
                       </div>
                     ))}
                     <button onClick={increaseOpenButtonClick}>Increase</button>
@@ -184,7 +210,7 @@ const AllOrder = () => {
                         <tr
                         className="border-b hover:text-white cursor-pointer"
                         key={item.id}
-                        onClick={() => handleRowClick(item.id)}
+                        onClick={() => handleDeliverClick(item.id)}
                         >
                             <div
                                 className={`p-0 pl-1 pr-20 w-180 h-24 items-start ${
