59fe239e6215eb9d5b49e7f57adf10cce535e2cc
Alexhtchen <b09902126@csie.ntu.edu.tw>
2023-11-25 13:04:18 +0800

    finish all ordering ui

--- src/containers/allOrder.js
+++ src/containers/allOrder.js
@@ -129,8 +129,7 @@ const AllOrder = () => {
 
     return (    
         <>
-            <NavBar/>
-            <div className="triangle-left"></div>      
+            <NavBar/>   
             <div className="flex p-10 pb-0 pt-4 items-center justify-center">
                 <h2 className="text-blue font-bold text-4xl">My Order</h2>
             </div>        
@@ -138,7 +137,7 @@ const AllOrder = () => {
                 <h2 className="text-grey font-bold text-2xl">Open</h2>
             </div>
             <div className="flex p-1 justify-around flex-wrap gap-6">
-                <button onClick={decreaseOpenButtonClick} className="button-with-left-triangle "></button>
+                <button onClick={decreaseOpenButtonClick} className="button-with-left-triangle"></button>
                 {filteredOpenOrder.map(item => (
                 <div key={item.id} className="flex flex-col items-start">
                     <div
