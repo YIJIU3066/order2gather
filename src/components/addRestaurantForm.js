92456c5f5127dacae242db86db5e1360ce245667
YIJIU3066 <yijiu3066@gmail.com>
2023-11-25 20:29:04 +0800

    1. add sweetalert for delete menu 2. can switch to prev and next when check menu at full screen

--- src/components/addRestaurantForm.js
+++ src/components/addRestaurantForm.js
@@ -2,7 +2,12 @@ import React, { useState, useRef, useEffect } from "react";
 import styles from "../styles/form.module.css";
 import styles_img from "../styles/addRestaurant.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUpload, faXmark } from "@fortawesome/free-solid-svg-icons";
+import {
+  faUpload,
+  faXmark,
+  faAngleRight,
+  faAngleLeft,
+} from "@fortawesome/free-solid-svg-icons";
 import Swal from "sweetalert2";
 
 const AddRestaurantForm = ({ onSave, onClose }) => {
@@ -30,6 +35,7 @@ const AddRestaurantForm = ({ onSave, onClose }) => {
     };
   }, [menus]);
 
+  // 選擇圖片
   const handleMenuChange = (event) => {
     const file = event.target.files[0];
     if (file && file.type.startsWith("image/")) {
@@ -46,6 +52,7 @@ const AddRestaurantForm = ({ onSave, onClose }) => {
     }
   };
 
+  // 上傳圖片
   const handleMenuUpload = () => {
     fileInputRef.current.click();
     if (menus) {
@@ -55,22 +62,73 @@ const AddRestaurantForm = ({ onSave, onClose }) => {
     }
   };
 
+  //刪除圖片
   const handleDeleteMenu = (indexToDelete) => {
-    const updatedMenus = [...menus];
-    updatedMenus.splice(indexToDelete, 1); // 刪除指定索引的菜單圖片
+    Swal.fire({
+      title: "Are you sure?",
+      text: "Do you want to delete this menu?",
+      icon: "warning",
+      iconColor: "#CF9546",
+      showCancelButton: true,
+      confirmButtonColor: "#7A989A",
+      cancelButtonColor: "#C67052",
+      confirmButtonText: "Yes, delete it!",
+      cancelButtonText: "Cancel",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        const updatedMenus = [...menus];
+        updatedMenus.splice(indexToDelete, 1); 
 
-    // 更新 state 中的 menus
-    setMenus(updatedMenus);
+        // 更新 state 中的 menus
+        setMenus(updatedMenus);
 
-    // 釋放被刪除的圖片的 URL
-    const updatedMenuURLs = [...menuURLs];
-    URL.revokeObjectURL(updatedMenuURLs[indexToDelete]); // 釋放被刪除圖片的 URL
-    updatedMenuURLs.splice(indexToDelete, 1);
+        // 释放被删除的图片的 URL
+        const updatedMenuURLs = [...menuURLs];
+        URL.revokeObjectURL(updatedMenuURLs[indexToDelete]);
+        updatedMenuURLs.splice(indexToDelete, 1);
 
-    // 更新 state 中的 menuURLs
-    setMenuURLs(updatedMenuURLs);
+        // 更新 state 中的 menuURLs
+        setMenuURLs(updatedMenuURLs);
+
+        Swal.fire({
+          title: "Deleted!",
+          text: "Your menu has been deleted.",
+          icon: "success",
+          iconColor: "#CF9546",
+          confirmButtonColor: "#7A989A",
+        });
+      }
+    });
   };
 
+  // 點擊圖片全螢幕
+  const handleFullScreen = (index) => {
+    setFullscreenImageIndex(index);
+  };
+
+  // 點擊關閉全螢幕
+  const handleCloseFullScreen = () => {
+    setFullscreenImageIndex(null);
+  };
+
+  // 前一張全螢幕
+  const handlePrevImage = () => {
+    if (fullscreenImageIndex !== null && fullscreenImageIndex > 0) {
+      setFullscreenImageIndex(fullscreenImageIndex - 1);
+    }
+  };
+
+  // 下一張全螢幕
+  const handleNextImage = () => {
+    if (
+      fullscreenImageIndex !== null &&
+      fullscreenImageIndex < menuURLs.length - 1
+    ) {
+      setFullscreenImageIndex(fullscreenImageIndex + 1);
+    }
+  };
+
+  // 儲存餐廳資料
   const handleSave = () => {
     // 檢查表單是否有效
     if (name && telephone) {
@@ -106,21 +164,13 @@ const AddRestaurantForm = ({ onSave, onClose }) => {
     }
   };
 
-  //點擊圖片全螢幕
-  const handleFullScreen = (index) => {
-    setFullscreenImageIndex(index);
-  };
-
-  const handleCloseFullScreen = () => {
-    setFullscreenImageIndex(null);
-  };
-
+  // 關閉新增餐廳視窗
   const handleClose = () => {
     onClose();
   };
 
   return (
-    <div className="restaurant_form_container w-full h-full flex justify-center items-center">
+    <div className="restaurant_form_container w-full h-full flex justify-center items-center overflow-auto min-h-fit">
       <div className="w-fit border-8 px-20 border-blue p-4 rounded shadow flex flex-col items-center bg-slate-50 relative">
         <div
           className="cursor-pointer absolute top-0 right-0 px-6 py-4"
@@ -227,7 +277,7 @@ const AddRestaurantForm = ({ onSave, onClose }) => {
                           >
                             <div className="relative w-full h-full">
                               <button
-                                className="cursor-pointer bg-transport bg-red/[0.8] hover:bg-yellow/[0.8] w-4 h-4 shadow-md rounded-full absolute -top-2 -right-2 flex justify-center items-center"
+                                className="cursor-pointer bg-transport bg-blue/[0.8] hover:bg-yellow/[0.8] w-4 h-4 hover:w-5 hover:h-5 hover:-top-2.5 hover:-right-2.5 shadow-md rounded-full absolute -top-2 -right-2 flex justify-center items-center"
                                 onClick={() => handleDeleteMenu(index)}
                               >
                                 <FontAwesomeIcon
@@ -248,13 +298,69 @@ const AddRestaurantForm = ({ onSave, onClose }) => {
                             {fullscreenImageIndex === index && (
                               <div
                                 className={`${styles_img.zoom_img_container}`}
-                                onClick={handleCloseFullScreen}
+                                onClick={(e) => {
+                                  handleCloseFullScreen(); // 關閉全螢幕
+                                }}
                               >
-                                <img
-                                  src={menuSrc}
-                                  alt="Fullscreen"
-                                  className={`${styles_img.zoom_img}`}
-                                />
+                                {/* 前一張 */}
+                                <button
+                                  style={{
+                                    visibility:
+                                      index !== 0 ? "visible" : "hidden",
+                                  }}
+                                  className="text-white z-50 hover:bg-gray-600/[0.7] w-10 h-10 mr-3 rounded-full shadow-md flex justify-center items-center"
+                                  onClick={(e) => {
+                                    e.stopPropagation();
+                                    handlePrevImage();
+                                  }}
+                                >
+                                  <FontAwesomeIcon
+                                    icon={faAngleLeft}
+                                    size="lg"
+                                  />
+                                </button>
+                                <div className="relative flex justify-center w-10/12">
+                                  {/* 圖片 */}
+                                  <button
+                                    className="cursor-pointer bg-transport  hover:bg-grey/[0.8] w-10 h-10 rounded-full absolute top-2 right-2 flex justify-center items-center"
+                                    onClick={(e) => handleCloseFullScreen()}
+                                  >
+                                    <FontAwesomeIcon
+                                      icon={faXmark}
+                                      size="lg"
+                                      style={{ color: "#fff" }}
+                                      className="faXmark_full"
+                                    />
+                                  </button>
+                                  <img
+                                    src={menuSrc}
+                                    alt="Fullscreen"
+                                    className={`${styles_img.zoom_img}`}
+                                    onClick={(e) => {
+                                      e.stopPropagation();
+                                    }}
+                                  />
+                                </div>
+
+                                {/* 下一張 */}
+                                <button
+                                  style={{
+                                    visibility:
+                                      index !== menuURLs.length - 1
+                                        ? "visible"
+                                        : "hidden",
+                                  }}
+                                  className="text-white z-50 hover:bg-gray-600 bg-gray-700/[0.6] w-10 h-10 ml-3 rounded-full shadow-md flex justify-center items-center"
+                                  onClick={(e) => {
+                                    e.stopPropagation();
+                                    handleNextImage();
+                                  }}
+                                >
+                                  <FontAwesomeIcon
+                                    icon={faAngleRight}
+                                    size="lg"
+                                  />
+                                </button>
                               </div>
                             )}
                           </div>
