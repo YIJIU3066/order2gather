c2eb6e10565416b9af67ffe7eb05afc9eeebc0f5
Alexhtchen <b09902126@csie.ntu.edu.tw>
2023-12-02 17:10:32 +0800

    add link to login if not login

--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -5,9 +5,12 @@ import { useContext } from 'react';
 import AuthContext from '../context/AuthContext';
 
 const useAxios = () => {
-  const { accessToken, user, logoutUser } = useContext(AuthContext)
-  const baseUrl = (window.location.origin === "http://localhost:3000" ? "http://localhost:8080" : window.location.origin)
-  
+  const { accessToken, user, logoutUser } = useContext(AuthContext);
+  const baseUrl =
+    window.location.origin === 'http://localhost:3000'
+      ? 'http://localhost:8080'
+      : window.location.origin;
+
   const axiosInstance = axios.create({
     baseURL: baseUrl,
     headers: { authorization: `Bearer ${accessToken}` },
